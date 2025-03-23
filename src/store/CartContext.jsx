211850f54c66bfe1f7101d1379d5043fa8b066c0
OmarShehab <omar.shehab121@gmail.com>
2025-03-23 04:24:12 +0200

    CartContext continuation

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -6,9 +6,60 @@ const CartContext = createContext({
     removeItem: (id)=>{}
 })
 
+function cartReducer(state,action){
+    if (action.type === 'ADD_ITEM'){
+       const existingItemIndex = state.items.findIndex((item)=> item.id === action.item.id)
+       const updatedItems = [...state.items]
+       if (existingItemIndex > -1){
+            const existingItem = state.items[existingItemIndex]
+            const updatedItem= {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            }
+            updatedItems[existingItemIndex] = updatedItem
+       } else{
+            updatedItems.push({...action.item, quantity: 1})
+       }
+       return {
+        ...state,
+        items: updatedItems,
+       }
+    }
+
+    if (action.type === 'REMOVE_ITEM'){
+        const existingItemIndex = state.items.findIndex((item)=> item.id === action.id)
+        const existingItem = state.item[existingItemIndex]
+        const updatedItems = [...state.items]
+        if(existingItem.quantity === 1){
+            updatedItems.splice(existingItemIndex, 1)
+        }
+        else {
+            const updatedItem = {...existingItem,
+            quantity: existingItem.quantity-1,    
+            }
+            updatedItems [existingItemIndex] = updatedItem
+
+        }
+           return {...state, items: updatedItems} 
+    }
+
+    return state;
+}
+
 export function CartContextProvider({children}){
-    useReducer()
-    return <CartContext.Provider>{children}</CartContext.Provider>
+    const [cart, dispatchCartAction ] = useReducer(cartReducer,{items:[]})
+    function addItem(item){
+        dispatchCartAction({type:'ADD_ITEM'},item)
+    }
+    function removeItem(id){
+        dispatchCartAction({type:'REMOVE_ITEM'},id)
+    }
+    const cartContext = {
+        items: cart.items,
+        addItem,
+        removeItem,
+    }
+    return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
 }
 
 export default CartContext;
