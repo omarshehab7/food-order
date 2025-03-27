cedfd93056fbaec853104d9d82cc559168ffefff
OmarShehab <omar.shehab121@gmail.com>
2025-03-27 07:10:00 +0200

    Added Checkout Component and modified userProgressCtx logic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,10 +10,14 @@ export default function Cart (){
     const cartCtx = useContext(CartContext)
     const userProgressCtx = useContext(UserProgressContext)
     const total = cartCtx.items.reduce((totalPrice,item) => totalPrice + item.quantity * item.price ,0)
+    const quantity = cartCtx.items.length
     function handleCloseCart(){
         userProgressCtx.hideCart()
     }
-    return ( <Modal className='cart' open={userProgressCtx.progress === 'cart'}>
+    function handleCheckout(){
+        userProgressCtx.showCheckout()
+    }
+    return ( <Modal className='cart' open={userProgressCtx.progress === 'cart'} onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}>
         <h2>Your Cart</h2>
         <ul>
             {cartCtx.items.map((item)=> 
@@ -25,7 +29,8 @@ export default function Cart (){
         <p className="cart-total">{currencyFormatter.format(total)}</p>
         <p className="modal-actions">
             <Button textOnly onClick={handleCloseCart}>Close</Button>
-            <Button onClick={handleCloseCart}>Go to Checkout</Button>
-        </p>
+            { quantity > 0 &&
+            <Button onClick={handleCheckout}>Go to Checkout</Button>
+             } </p>
     </Modal>)
 }
